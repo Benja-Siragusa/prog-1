97de834fb37c667df05a225b81a9c94dff6ba91d
benjamin <benja.siragusa.2003@gmail.com>
2024-09-04 18:38:33 -0300

    uploading act3 unit1 completed

--- src/unidad1/actProceso/actividad3/act3_3.java
+++ src/unidad1/actProceso/actividad3/act3_3.java
@@ -11,43 +11,57 @@ distintas opciones:
     - salir
 Utilice la librería JOptionPane. */
 
+
 public class act3_3 {
     public static void main(String[] args) {
+        try {
+            double n = pedirNumero();
+            menu(opcion(), n);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "el programa solo acepta numeros", "error", JOptionPane.ERROR_MESSAGE);
+        }
+
     }
 
-    public static void menu(int n) {
+    public static void menu(int n, double a) {
         switch (n) {
+            case 0:
+                JOptionPane.showMessageDialog(null, "la medida ingresada es de " + a + "m", "medida en metros",
+                        JOptionPane.INFORMATION_MESSAGE);
+                break;
             case 1:
-                
+                JOptionPane.showMessageDialog(null,
+                        "la medida en metros ingresada convertida a kilometros es de " + a / 1000 + "km",
+                        "medida en kilometros", JOptionPane.INFORMATION_MESSAGE);
                 break;
             case 2:
-
+                JOptionPane.showMessageDialog(null,
+                        "la medida en metros ingresada convertida a centimetros es de " + a * 100 + "cm",
+                        "medida en centimetros", JOptionPane.INFORMATION_MESSAGE);
                 break;
             case 3:
-
+                JOptionPane.showMessageDialog(null,
+                        "la medida en metros ingresada convertida a milimetros es de " + a * 1000 + "mm",
+                        "medida en centimetros", JOptionPane.INFORMATION_MESSAGE);
                 break;
             case 4:
-
-                break;
-            case 5:
-
-                break;
-
-            default:
                 break;
         }
     }
 
     public static int opcion() {
         Object[] options = { "metros", "convertir a km", "convertir a cm", "convertir a mm", "salir" };
-        int seleccion = JOptionPane.showOptionDialog(null, "seleccione una opvion", "opciones", 0,
+        int seleccion = JOptionPane.showOptionDialog(null, "seleccione una opcion", "opciones", 0,
                 JOptionPane.QUESTION_MESSAGE, null, options, options[0]);
         return seleccion;
     }
 
-    public static int pedirNumero() {
-        int numero = Integer.parseInt(JOptionPane.showInputDialog(null, "ingrese su calificacion",
-                "subida de calificaciones", JOptionPane.QUESTION_MESSAGE));
+    public static double pedirNumero() {
+
+        double numero = Double.parseDouble(JOptionPane.showInputDialog(null, "ingrese una medida en metros",
+                "pedir medida", JOptionPane.QUESTION_MESSAGE));
+
         return numero;
+
     }
 }
